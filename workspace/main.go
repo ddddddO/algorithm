6d03ef454e3dd88ec43ae389a78a9421c45b9035
dddddO <lbfdeatq@gmail.com>
2018-12-23 18:58:33 +0900

    add workspace

---
+++ workspace/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ddddddO/algorithm/workspace/ws"
+)
+/*
+横道にそれたプログラム試す用
+横道プログラムの実装は、
+"github.com/ddddddO/algorithm/workspace/ws"　で。
+*/
+func main() {
+	fmt.Println("- start -")
+
+	fmt.Println(ws.Fibonacci(9))
+
+	fmt.Println("-  end  -")
+}
