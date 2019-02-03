c5c7c488037ac3f0614ffaa716b5c8f4ee9afcd0
dddddO <lbfdeatq@gmail.com>
2019-02-04 01:43:25 +0900

    fix

--- tasks/capter4/insertion_sort.py
+++ tasks/capter4/insertion_sort.py
@@ -3,13 +3,23 @@ def insert_sort(l):
     if len(l) < 1:
         return
 
+    exch_sum = 0
     for i in range(len(l)-1):
         tmp = l[i+1] # 整列済み部分の１つ後ろの要素
+        exch_n = 0
 
         # 整列済み部分の後ろからtmpを挿入するところをサーチ
         for j in range(i, -1, -1):
+            exch_n+=1
+
             if l[j] >= tmp:
                 l[j], l[j+1] = tmp, l[j]
+            else:
+                # 残りの整列済み部分のサーチを飛ばす
+                break
+
+        exch_sum+= exch_n
+    print("比較回数(合計)：{}".format(exch_sum))
 
 if __name__ == '__main__':
     print("--insertion sort start--")
